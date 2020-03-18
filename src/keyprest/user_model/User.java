059c61c74d5646378f6dce8100839b82d7ac7344
Andrea Santaniello <andrea@napalm.rocks>
2020-03-18 18:30:58 +0100

    primi lavori login

--- src/keyprest/user_model/User.java
+++ src/keyprest/user_model/User.java
@@ -4,5 +4,24 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6166767199952834976L;
+
+	private int UserID;
+	private String Username;
+	private String HashedPassword;
+	private String RealName;
+	private String BillingAddress;
+	private float CreditsBalance;
 	
+	public int getUserID() { return UserID; }
+	public void setUserID(int UserID) { this.UserID = UserID; }
+	
+	public String getUsername() { return Username; }
+	public void setUsername() 
+	
+	
+	}
 }
--- src/keyprest/user_model/login_handler.java
+++ src/keyprest/user_model/login_handler.java
@@ -1,20 +1,59 @@
 package keyprest.user_model;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
+import javax.servlet.SessionCookieConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.Session;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import keyprest.database.connectionManager;
+
 public class login_handler extends HttpServlet {
 
+	String GLOBAL_SALT = ""; /* TODO: questo deve essere spostato*/
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String Username = request.getParameter("username");
 		String Password = request.getParameter("password");
-		String EMail = request.getParameter("e-mail");
+		/* TODO:  finis
+		try {
+			if(doLogin(Username, Password)) { Session.setAttribute("username", Username);}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public boolean doLogin(String Username, String Password) throws ClassNotFoundException, SQLException
+	{
+		String QUERY = "SELECT * FROM users" + 
+				"         WHERE EXISTS(select 1 from users where username = ? OR mailaddr = ? )" + 
+				"               AND password = ?";
+		
+		if(Username.isEmpty() || Password.isEmpty()) { return false; }
+		
+		String HashedPassword = DigestUtils.sha256Hex(Password + GLOBAL_SALT); 
+		
+		if(connectionManager.databaseConnection.isClosed()) { connectionManager.createConnection(); }
+		
+		PreparedStatement preparedStatement = connectionManager.databaseConnection.prepareStatement(QUERY);
+		
+		preparedStatement.setString(1, Username);
+		preparedStatement.setString(2, Username);
+		preparedStatement.setString(3, HashedPassword);
+		
+		return preparedStatement.execute();
 		
 	}
 	
