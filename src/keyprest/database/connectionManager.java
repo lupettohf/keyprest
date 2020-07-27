25a66d80d2778f54e4daefa94389a792896680b3
Andrea Santaniello <andrea@napalm.rocks>
2020-07-27 17:40:21 +0200

    Revert "."
    This reverts commit 06d701406d589d173b59177336f33c445b87a9e7.

--- src/keyprest/database/connectionManager.java
+++ src/keyprest/database/connectionManager.java
@@ -17,7 +17,6 @@ public class connectionManager {
         config.setJdbcUrl( "jdbc:mysql://localhost:3306/keyprest" );
         config.setUsername( "keyprest" );
         config.setPassword( "PorcodioPorcamadonna" );
-        config.addDataSourceProperty("dataSourceClassName", "com.mysql.cj.jdbc.Driver");
         ds = new HikariDataSource( config );
     }
 	
