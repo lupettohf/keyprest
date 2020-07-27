ae2ca7c284d70ee0eb425f4960556c0c194741cd
Andrea Santaniello <andrea@napalm.rocks>
2020-07-27 18:32:09 +0200

    .

--- demoKeys.py
+++ demoKeys.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import string
-import random 
+import random
 
 def randomSegKey():
     letters = string.ascii_uppercase
@@ -8,26 +8,26 @@ def randomSegKey():
 
 def randomKey():
     return randomSegKey() +'-'+ randomSegKey() +'-'+ randomSegKey() +'-'+ randomSegKey()
-    
+
 def populateTable(ProductID, Quantity):
     QUERY = "INSERT INTO `keys` (`product_id`, `key`, `sold`) VALUES (%i,%s,0)"
-    
+
     db = mysql.connector.connect(
         host="192.168.1.200",
         user="keyprest",
-        password="PorcodioPorckeyprestnna",
+        password="keyprest",
         database="keyprest"
     )
 
     cursor = db.cursor()
-    
+
     for i in range(0, Quantity):
         values = (ProductID, randomKey())
         cursor.execute(QUERY, values)
         db.commit()
-        
-        print("Added row:", cursor.lastrowid) 
-        
+
+        print("Added row:", cursor.lastrowid)
+
     print("end.")
 
-populateTable(1, 90)    
+populateTable(1, 90)
